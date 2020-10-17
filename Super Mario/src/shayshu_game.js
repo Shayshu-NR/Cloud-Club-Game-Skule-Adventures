fde7bf3bdb5dd3a56d5300f9f724ac4a03bd0178
Shayshu Nahata <55864616+Shayshu-NR@users.noreply.github.com>
2020-10-16 20:08:23 -0400

    Added side scrolling example

--- Super Mario/src/shayshu_game.js
+++ Super Mario/src/shayshu_game.js
@@ -16,38 +16,23 @@ let cursors
 let player
 var text;
 var timedEvent;
-var sky
-var json_parsed
 
 function preload() {
     // Load & Define our game assets
     game.load.image('sky', './assets/sky.png')
     game.load.image('ground', './assets/platform.png')
     game.load.image('diamond', './assets/diamond.png')
-    game.load.spritesheet('woof', './assets/woof.png', 32, 32)
-    game.load.spritesheet('goomba', './assets/mimic.png', 32, 32)
+    game.load.spritesheet('eng', './assets/Main sprite.png', 32, 32)
+    game.load.image('steve', './assets/steve.png')
     game.load.audio("mario_die", './assets/smb_mariodie.wav')
-    game.load.spritesheet("spike", "./assets/spike.png", 32, 32)
-    game.load.spritesheet("brick", "./assets/brick.png", 32, 32)
-
-    game.load.text("Json_test", "./data/test.json")
 }
 
 function create() {
-    json_parsed = JSON.parse(game.cache.getText("Json_test"))
-    console.log(json_parsed)
-
-
     //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE)
 
-
-
-    //~~~~~ Tiling the background ~~~~~
-    sky = game.add.tileSprite(0, 0, 800, 600, 'sky')
-    sky.fixedToCamera = true
-    sky.tilePosition.x = game.camera.x * -0.2
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //  A simple background for our game
+    game.add.sprite(0, 0, 'sky')
 
     //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = game.add.group()
@@ -56,10 +41,10 @@ function create() {
     platforms.enableBody = true
 
     // Here we create the ground.
-    const ground = platforms.create(0, game.world.height - 32, 'ground')
+    const ground = platforms.create(0, game.world.height - 64, 'ground')
 
     //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(20, 2)
+    ground.scale.setTo(2, 2)
 
     //  This stops it from falling away when you jump on it
     ground.body.immovable = true
@@ -68,12 +53,11 @@ function create() {
     let ledge = platforms.create(400, 450, 'ground')
     ledge.body.immovable = true
 
-
     ledge = platforms.create(-75, 350, 'ground')
     ledge.body.immovable = true
 
     // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'woof')
+    player = game.add.sprite(32, game.world.height - 150, 'eng')
 
     //  We need to enable physics on the player
     game.physics.arcade.enable(player)
@@ -84,8 +68,8 @@ function create() {
     player.body.collideWorldBounds = true
 
     //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1], 10, true)
-    player.animations.add('right', [2, 3], 10, true)
+    player.animations.add('left', [1, 2], 10, true)
+    player.animations.add('right', [1, 0], 10, true)
 
     //  Finally some diamonds to collect
     diamonds = game.add.group()
@@ -102,6 +86,14 @@ function create() {
         diamond.body.bounce.y = 0.3 + Math.random() * 0.2
     }
 
+    //Adding an enemey to the level
+    enemy = game.add.group();
+
+    enemy.enableBody = true;
+
+    const steve = enemy.create(350, 350, 'steve');
+
+
     //  Create the score text
     scoreText = game.add.text(16, 16, '', { fontSize: '32px', fill: '#000' })
 
@@ -115,41 +107,13 @@ function create() {
     //Creating a timer...
     // 2:30 in seconds
     this.timeLimit = 30;
-    this.timeText = game.add.text(15, 50, "00:00");
+    this.timeText = game.add.text(700, 20, "00:00");
     this.timeText.fill = "#000000";
     this.timer = game.time.events.loop(1000, tick, this);
-    //Start the timer once everyting is loaded...
-    //~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    //~~~~~ Demo of moving enemy ~~~~~
-    hazard = game.add.group()
-    hazard.enableBody = true
-
-    const spike = hazard.create(525, 420, 'spike')
-    spike.body.gravity = 1000
-    spike.body.immovable = true
-
-    enemy1 = hazard.create(100, 300, 'goomba')
-    enemy1.animations.add('fly', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 6, true)
-    enemy1.animations.play('fly')
-
-    tween1 = game.add.tween(enemy1)
-    tween1.loop = -1
-    tween1.to({ x: 350, y: 300 }, 2000, null, true, 0, loop = 100, true)
-
-    console.log(this);
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    //~~~~~ Demo of birck ~~~~~
-    brick = game.add.group()
-    brick.enableBody = true
-
-    const block = brick.create(50, game.world.height - 150, 'brick')
-    block.body.immovable = true
-
-    game.world.setBounds(0, 0, 8000, 600)
-    game.camera.follow(player);
-    //~~~~~~~~~~~~~~~~~~~~~~~~~
+    console.log(this)
+        //Start the timer once everyting is loaded...
+        //~~~~~~~~~~~~~~~~~~~~~~~~~
 }
 
 function update() {
@@ -159,12 +123,13 @@ function update() {
     //  Setup collisions for the player, diamonds, and our platforms
     game.physics.arcade.collide(player, platforms)
     game.physics.arcade.collide(diamonds, platforms)
-    game.physics.arcade.collide(hazard, platforms)
-    game.physics.arcade.collide(player, hazard, kill_mario, null, this)
-    game.physics.arcade.collide(player, brick, brick_break, null, this)
+    game.physics.arcade.collide(enemy, platforms)
 
     //  Call callectionDiamond() if player overlaps with a diamond
     game.physics.arcade.overlap(player, diamonds, collectDiamond, null, this)
+
+    //
+    game.physics.arcade.overlap(player, enemy, kill_mario, null, this);
 
     // Configure the controls!
     if (cursors.left.isDown) {
@@ -198,6 +163,18 @@ function collectDiamond(player, diamond) {
     scoreText.text = 'Score: ' + score
 }
 
+function kill_mario(player, enemy) {
+    enemy.kill();
+    player.kill();
+
+    var die_noise = game.add.audio("mario_die");
+    die_noise.play();
+
+    alert("Game over");
+
+    location.reload();
+}
+
 var tick = function() {
     this.timeLimit--;
     var minutes = Math.floor(this.timeLimit / 60);
@@ -221,41 +198,4 @@ var outofTime = function() {
     die_noise.play();
     alert("Out of Time!");
     location.reload();
-}
-
-function kill_mario(player, enemy) {
-    player.kill();
-
-    var die_noise = game.add.audio("mario_die");
-    die_noise.play();
-
-    alert("Game over");
-
-    location.reload();
-}
-
-
-function brick_break(player, block) {
-    //Only break the brick when the player is below 
-    //and not hittin gon the sides
-    console.log('Player (x,y):', "(", player.position.x, player.position.y, ")")
-    console.log('Block (x,y):', "(", block.position.x, block.position.y, ")")
-
-    var player_x = player.position.x
-    var player_y = player.position.y
-
-    var block_x = block.position.x
-    var block_y = block.position.y
-
-    if (player_y < block_y) {
-        return
-    } else if (player_x > block_x + 16) {
-        return
-    } else if (player_x < block_x - 16) {
-        return
-    } else {
-        block.kill()
-    }
-
-
 }
